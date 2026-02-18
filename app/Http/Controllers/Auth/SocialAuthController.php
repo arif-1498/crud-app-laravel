9c4acf80fdf3e7c0ca501cb8af770fa4f8795094
[Arif <arif.hussoun898@gmail.com>
2026-02-18 16:38:59 +0500

    fixed: social controller issue fixed

--- app/Http/Controllers/Auth/SocialAuthController.php
+++ app/Http/Controllers/Auth/SocialAuthController.php
@@ -12,35 +12,30 @@ use App\Providers\RouteServiceProvider;
 
 class SocialAuthController extends Controller
 {
- 
-public function redirect($social){
-       return Socialite::driver($social)->redirect();
-       
- }
- public function callback(Request $request, $social){
-    $socialUser = Socialite::driver($social )->user();
-    $user=User::where('social_id',$socialUser->getId())->first();
-    
-    if(!$user){
-      User::create([
-         'social_id'=>$socialUser->getId(),
-         'social_platform'=>'Google',
-         'name'=>$socialUser->getName(),
-         'email'=>$socialUser->getEmail(),
-         'password'=>Hash::make('password'),
 
-      ]);
-        Auth::login($user);
-        $request->session()->regenerate();
-       return redirect()->intended(RouteServiceProvider::HOME);
-    }
-    else{
-      Auth::login($user);
-      $request->session()->regenerate();
-       return redirect()->intended(RouteServiceProvider::HOME);
-    }
-   
-    
- 
- }
-}
+   public function redirect($social)
+   {
+      return Socialite::driver($social)->redirect();
+   }
+   public function callback(Request $request, $social)
+   {
+      $socialUser = Socialite::driver($social)->user();
+      $suser = User::firstOrCreate(
+         ['social_id' => $socialUser->getId()],
+         [
+            'social_platform' => 'Google',
+            'name' => $socialUser->getName(),
+            'email' => $socialUser->getEmail(),
+            'password' => Hash::make('password'),
+         ]
+      );
+
+         Auth::login($suser);
+         $request->session()->regenerate();
+         return redirect()->intended(RouteServiceProvider::HOME);
+      }
+
+
+
+   }
+
